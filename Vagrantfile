108cd53a9e0d7038915038e45a78e59ba3d9083a
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2018-02-22 13:50:55 +0000

    changes to debian

--- Vagrantfile
+++ Vagrantfile
@@ -1,13 +1,12 @@
 Vagrant.configure(2) do |config|
 
   config.vm.network :private_network, ip: "192.168.58.111"
-  config.vm.box = "ubuntu/xenial64"
+  config.vm.box = "debian/stretch64"
   
   config.vm.provider "virtualbox" do |vb|
     vb.memory = "1524"
   end
   config.vm.provision :shell, path: "bootstrap.sh"
   config.vm.synced_folder "./", "/var/www"
-
 
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,42 @@
 #!/usr/bin/env bash
 
-apt-get update -y
-apt-get upgrade -y
-apt-get install nginx -y
-apt-get install build-essential -y
-apt-get install python-pip -y
-pip install --upgrade pip
-pip install -U setuptools
+#apt-get update -y
+#apt-get upgrade -y
+#apt-get install nginx -y
+#apt-get install build-essential -y
+#apt-get install python-pip -y
+#pip install --upgrade pip
+#pip install -U setuptools
+
+
+export APP_DIR=/app
+export DEP_NODE_VERSION=6.12.2
+export DEP_YARN_VERSION=1.3.2-1
+export DEP_SUPERVISOR_VERSION=3.3.3
+export DEP_UWSGI_VERSION=2.0.15
+export DEP_AWSCLI_VERSION=1.14.31
+export DEP_AWSCLI_CWLOGS_VERSION=1.4.4
+
+apt-get update && \
+apt-get install -y --no-install-recommends nginx \
+                libpcre3-dev libpq-dev libffi-dev libxml2-dev libxslt-dev python-setuptools libssl-dev \
+                gcc make git curl xz-utils python2.7 python-pip python-virtualenv apt-transport-https gnupg python-pip && \
+curl -SLO "https://nodejs.org/dist/v${DEP_NODE_VERSION}/node-v${DEP_NODE_VERSION}-linux-x64.tar.xz" && \
+tar -xJf "node-v${DEP_NODE_VERSION}-linux-x64.tar.xz" -C /usr/local --strip-components=1 && \
+rm "node-v${DEP_NODE_VERSION}-linux-x64.tar.xz" && \
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && \
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list && \
+apt-get update && \
+apt-get install -y --no-install-recommends yarn=${DEP_YARN_VERSION} && \
+rm -rf /var/lib/apt/lists/* && \
+/usr/bin/python2.7 /usr/bin/pip install supervisor==${DEP_SUPERVISOR_VERSION} && \
+/usr/local/bin/pip3 install --no-cache-dir uwsgi==${DEP_UWSGI_VERSION} awscli==${DEP_AWSCLI_VERSION} awscli-cwlogs==${DEP_AWSCLI_CWLOGS_VERSION} && \
+aws configure set plugins.cwlogs cwlogs && \
+mkdir -p ${APP_DIR} && \
+rm -f /etc/nginx/sites-enabled/* && \
+mkdir -p /var/log/digitalmarketplace && \
+chmod 777 /run
+
 
 # NGINX
 local_config="/var/www/digitalmarketplace-functional-tests/nginx/nginx.conf"
